cc00a2ebc5d1d8e4066f0bfc425076feb528ee79
Hanan Beer <email@hanan.beer>
2022-10-07 20:00:18 -0500

    updates

--- src/Zerem.sol
+++ src/Zerem.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "./IERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract Zerem {
     address public underlyingToken;
--- test/Zerem.t.sol
+++ test/Zerem.t.sol
@@ -4,19 +4,20 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 import "../src/Zerem.sol";
+import "./MockERC20.sol";
 
 contract ZeremTest is Test {
     Zerem public zerem;
     function setUp() public {
         // usdc
-        address underlyingToken = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+        address underlyingToken = address(new MockERC20("mock", "mock", 1e28));
         uint256 minLockAmount = 1000e18; // 1k units
         uint256 unlockDelaySec = 24 hours;
         uint256 unlockPeriodSec = 48 hours;
 
        zerem = new Zerem(underlyingToken, minLockAmount, unlockDelaySec, unlockPeriodSec);
 
-        deal(zerem.underlyingToken(), address(this), 1000e18);
+        //deal(zerem.underlyingToken(), address(this), 1000e18);
     }
 
     function testTransferNoFunds() public {
@@ -31,23 +32,36 @@ contract ZeremTest is Test {
         vm.recordLogs();
         zerem.transferTo(address(this), amount);
         Vm.Log[] memory entries = vm.getRecordedLogs();
-        console.log(entries.length);
-        assertEq(entries.length, 1);
-        console.logBytes32(entries[0].topics[0]);
+        assertEq(entries.length, 2);
+        console.logBytes32(entries[1].topics[0]);
         console.logAddress(address(this));
-        console.logBytes32(entries[0].topics[1]);
-        return;
-        assertEq(entries[0].topics[0], keccak256("TransferFulfilled(address,uint256,uint256)"));
+        console.logBytes32(entries[1].topics[1]);
+        assertEq(entries[1].topics[0], keccak256("TransferFulfilled(address,uint256,uint256)"));
         assertEq(uint256(entries[0].topics[1]), uint256(uint160(address(this))));
-        return;
 
-        (uint256 withdrawnAmount, uint256 remainingAmount) = abi.decode(entries[0].data, (uint256, uint256));
+        (uint256 withdrawnAmount, uint256 remainingAmount) = abi.decode(entries[1].data, (uint256, uint256));
         assertEq(withdrawnAmount, amount);
         assertEq(remainingAmount, uint256(0));
     }
 
     function testTransferLock() public {
-        
+        uint256 amount = 1000e18;
+        IERC20(zerem.underlyingToken()).transfer(address(zerem), amount);
+
+        vm.recordLogs();
+        zerem.transferTo(address(this), amount);
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+
+        assertEq(entries.length, 1);
+        console.logBytes32(entries[0].topics[0]);
+        console.logAddress(address(this));
+        console.logBytes32(entries[0].topics[1]);
+        assertEq(entries[0].topics[0], keccak256("TransferLocked(address,uint256,uint256)"));
+        assertEq(uint256(entries[0].topics[1]), uint256(uint160(address(this))));
+
+        (uint256 amountLocked, uint256 lockTimestamp) = abi.decode(entries[0].data, (uint256, uint256));
+        assertEq(amountLocked, amount);
+        assertEq(lockTimestamp, uint256(block.timestamp));
     }
 
     function testTransferUnlock() public {
