e4098c33873c6ec78f501a779cf6e1d2f48b614d
Hanan Beer <email@hanan.beer>
2022-10-07 23:23:34 -0500

    add mock erc

---
+++ test/MockERC20.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: WTFPL
+pragma solidity 0.8.13;
+
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 supply
+    ) public ERC20(name, symbol) {
+        _mint(msg.sender, supply);
+    }
+}
