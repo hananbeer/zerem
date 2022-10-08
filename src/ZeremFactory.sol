021bff81b6a6e36eb906607b14ffe9dfa8337b59
ImmanuelSegol <3ditds@gmail.com>
2022-10-07 23:40:30 -0500

    feat: add factory

--- src/ZeremFactory.sol
+++ src/ZeremFactory.sol
@@ -2,12 +2,20 @@
 pragma solidity ^0.8.13;
 
 import "./Zerem.sol";
+import "./ZeremEther.sol";
 
 contract ZeremFactory {
     mapping (bytes32 => address) public zerems;
-    
     function deploy(address _token, uint256 _minLockAmount, uint256 _unlockDelaySec, uint256 _unlockPeriodSec) public returns (address) {
         bytes32 id = keccak256(abi.encode(msg.sender, _token)); //??}
+
+        if (_token == address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE)) {
+          ZeremEther zeremEth = new ZeremEther{ salt: id }(_token, _minLockAmount, _unlockDelaySec, _unlockPeriodSec);
+          zerems[id] = address(zeremEth);
+
+          return address(zeremEth);
+        }
+
         Zerem zerem = new Zerem{ salt: id }(_token, _minLockAmount, _unlockDelaySec, _unlockPeriodSec);
         zerems[id] = address(zerem);
 
