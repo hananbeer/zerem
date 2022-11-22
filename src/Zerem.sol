5fbd3ee6df7bededb1ba11d940b89d77bcce7fe0
high_byte <63053861+hananbeer@users.noreply.github.com>
2022-11-22 12:42:55 +0200

    Update Zerem.sol

--- src/Zerem.sol
+++ src/Zerem.sol
@@ -186,41 +186,6 @@ contract Zerem {
             withdrawableAmount = record.remainingAmount;
     }
 
-    function _getWithdrawableAmount_old(bytes32 transferId) internal view returns (uint256 withdrawableAmount) {
-        TransferRecord storage record = _getRecord(transferId);
-
-        // calculate unlock function
-        // in this case, we are using a delayed linear unlock:
-        // f(t) = amount * delta
-        // delta = clamp(now - lockTime + unlockDelay, 0%, 100%)
-        // for example, start delay of 24 hours and end delay of 72 hours/
-        // give us initial 24 hours period with no unlock, following 48 hours period
-        // of gradual unlocking
-        // need to normalize between 0..1
-        // so (deltaTime - startDelay) / (endDelay - startDelay) = (deltaDelayed / 48hr)
-        // then clamp 0..1
-
-        uint256 deltaTime = block.timestamp - record.lockTimestamp;
-        if (deltaTime < unlockDelaySec)
-            return 0;
-
-        uint256 deltaTimeDelayed = (deltaTime - unlockDelaySec);
-        if (deltaTimeDelayed >= unlockPeriodSec) {
-            withdrawableAmount = record.remainingAmount;
-        } else {
-            // calculate the total amount unlocked amount
-            uint256 totalUnlockedAmount = (record.totalAmount * 1e5 * deltaTimeDelayed) / (1e5 * unlockPeriodSec);
-            // subtract the already withdrawn amount from the unlocked amount
-            uint256 withdrawnAmount = record.totalAmount - record.remainingAmount;
-            if (totalUnlockedAmount < withdrawnAmount)
-                return 0;
-
-            withdrawableAmount = totalUnlockedAmount - withdrawnAmount;
-            if (withdrawableAmount > record.remainingAmount)
-                withdrawableAmount = record.remainingAmount;
-        }
-    }
-
     function getWithdrawableAmount(address user, uint256 lockTimestamp) public view returns (uint256 amount) {
         bytes32 transferId = _getTransferId(user, lockTimestamp);
         return _getWithdrawableAmount(transferId);
